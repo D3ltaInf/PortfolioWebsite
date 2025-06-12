96bd590bf517b372d0c1fbdf54ced7e1991341a0
D3ltaInf <129307231+D3ltaInf@users.noreply.github.com>
2025-06-12 02:02:41 -0700

    feat: Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // Tells Next.js to export a static site
+  output: 'export',
+
+  // Required for the site to work on GitHub Pages
+  // Replace `your-repo-name` with your repository's name
+  basePath: '/PortfolioWebsite',
+
+  // Disables image optimization, which is not supported in a static export
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
